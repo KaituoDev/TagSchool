29f66635f3f2d0e1657a8f822000805c00ce8587
YFshadaow <renzd2001@gmail.com>
2022-03-22 18:47:59 +0800

    Minor change

--- src/tech/yfshadaow/TagGame.java
+++ src/tech/yfshadaow/TagGame.java
@@ -73,7 +73,7 @@ public class TagGame extends Game implements Listener {
     }
     private TagGame(Tag plugin) {
         this.plugin = plugin;
-        initGame(plugin, "Tag", 5, new Location(world,-5, 203, -1002),BlockFace.NORTH,
+        initGame(plugin, "Tag","§c鬼抓人§f-学校", 5, new Location(world,-5, 203, -1002),BlockFace.NORTH,
                 new Location(world, -10,204,-1007),BlockFace.EAST,
                 new Location(world,-5, 202, -1007), new BoundingBox(-79, 85, -1083,0, 50, -1000));
         players = plugin.players;
